a52d3887ab2ac3f50c496683661b4ce6bc3c2ae4
vardhanghelani <142500847+vardhanghelani@users.noreply.github.com>
2024-07-10 10:58:11 +0530

    Update LAB_5(LIKE  OPERATOR).sql

--- LAB_5(LIKE  OPERATOR).sql
+++ LAB_5(LIKE  OPERATOR).sql
@@ -6,10 +6,15 @@ Website VARCHAR(50),
 City VARCHAR(25),
 Address VARCHAR(100))
  
- INSERT INTO STUDENT VALUES (1011 ,'Keyur Patel', 'techonthenet.com',' Rajkot', 'A-303 ''Vasant Kunj''', 'Rajkot')
-INSERT INTO STUDENT VALUES(1022 ,'Hardik Shah' ,'digminecraft.com ','Ahmedabad ','ìRam Krupaî', 'Raiya Road')
-INSERT INTO STUDENT VALUES(1033 ,'Kajal Trivedi ','bigactivities.com ','Baroda ','Raj bhavan plot', 'near garden')
-INSERT INTO STUDENT VALUES(1044 ,'Bhoomi Gajera ','checkyourmath.com', 'Ahmedabad', 'ìJigís Homeî',' Narol')
-INSERT INTO STUDENT VALUES (1055, 'Harmit Mitel', '@me.darshan.com ','Rajkot', 'B-55',' Raj Residency')
-INSERT INTO STUDENT VALUES(1066 ,'Ashok Jani', NULL, 'Baroda ','A502', 'Club House Building')
+ INSERT INTO STUDENT VALUES (1011 ,'Keyur',' Patel', 'techonthenet.com',' Rajkot', 'A-303 ''Vasant Kunj''')
+INSERT INTO STUDENT VALUES(1022 ,'Hardik', 'Shah' ,'digminecraft.com ','Ahmedabad ','‚ÄúRam Krupa‚Äù,RAIYA ROADF')
+INSERT INTO STUDENT VALUES(1033 ,'Kajal ','Trivedi ','bigactivities.com ','Baroda ','Raj bhavan plot,near garden')
+INSERT INTO STUDENT VALUES(1044 ,'Bhoomi',' Gajera ','checkyourmath.com', 'Ahmedabad', '‚ÄúJig‚Äôs Home‚Äù,Narol')
+INSERT INTO STUDENT VALUES (1055, 'Harmit',' Mitel', '@me.darshan.com ','Rajkot', 'B-55,Raj Residency')
+INSERT INTO STUDENT VALUES(1066 ,'Ashok ','Jani', NULL, 'Baroda ','A502,Club House Building')
 SELECT *FROM STUDENT
+truncate  table student
+--PART A
+SELECT FIRSTNAME FROM STUDENT WHERE FirstName LIKE 'K%'
+SELECT FIRSTNAME FROM STUDENT WHERE FirstName LIKE '_____'
+SELECT FIRSTNAME,LastName FROM STUDENT WHERE CITY LIKE '_____a'
